1b1eb05462a6d076fd0828c8dbe8b1e996e31b70
s-katte <shubhamkatte99@gmail.com>
2021-06-19 07:17:44 +0530

    update views and nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User < ActiveRecord::Base
     user_stocks.where(stock_id: stock.id).exists?
   end
 
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+
   def under_stock_limit?
     (user_stocks.count < 10)
   end
