d11db352bfcaefc4c0e046b321cb98889ba30d2c
s-katte <shubhamkatte99@gmail.com>
2021-06-20 11:22:40 +0530

    Complete the application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,35 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+    @friendships = current_user.friends
+  end
 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "Empty search query"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_users(@users)
+      flash.now[:danger] = "No user found" if @users.blank?
+    end
+    render partial: 'friends/result'
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    
+    if current_user.save
+      flash[:success] = "Friend was succefully added"
+    else
+      flash[:danger] = "Something went wrong"
+    end
+    redirect_to my_friends_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
   end
 
 end
